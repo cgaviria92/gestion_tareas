208a6fce118735cf1f5a34540371632e23daed30
carlos gaviria <ing.carlos.gaviria@gmail.com>
2024-05-20 13:30:55 -0500

    django material admin

--- gestion_tareas/settings.py
+++ gestion_tareas/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    #'django.contrib.admin', estilos viejos
+    'material',
+    'material.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
